d12e9752e62c877029a5e22df31ce26d2074bc3d
diceboi <szasz.szabolcs1995@gmail.com>
2026-05-29 00:07:03 +0200

    private folder download

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+    experimental: {
+        outputFileTracingIncludes: {
+            '/api/download': ['./private/**/*'],
+        },
+    },
     images: {
         remotePatterns: [
             {
